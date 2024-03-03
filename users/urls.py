67a8325c689fd8a1a074a8de12aab583707b1bf8
rems <tech1.rems@gmail.com>
2024-03-03 23:20:35 +0000

    Recreated the user registration to add email field. Also added user and profile update forms and render on profile page. Change the go back button texts to Cancel. Added requirements for the project. Other changes

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
-from .views import SignUpView
 from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', views.registerUser, name='signup'),
     path('profile/', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,19 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 
 # Create your views here.
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'users/signup.html'
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'users/signup.html'
 
 
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created successfully! You can now login.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Sorry! Your have entered an invalid detail')
+            return redirect('signup')
+    else:
+        form = UserRegisterForm()
+    context = {'form':form}
+    return render(request, 'users/signup.html', context)
 
 
 
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Sorry! Your have entered an invalid detail')
+            return redirect('profile')
+    user_form = UserUpdateForm(instance=request.user)
+    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {'user_form':user_form, 'profile_form':profile_form}
+    return render(request, 'users/profile.html', context)
